c8275748fbfb728805f5c92203420fdf41d83280
Hallman <mhallman@elc-l07h50y34y.lan>
2023-12-01 13:38:01 -0500

    adding github links to all projects

--- projects/live-your-life/index.php
+++ projects/live-your-life/index.php
@@ -30,7 +30,7 @@
     <h2>More Info...</h2>
     <div class="row section">
       <div class="col-xs-12">
-        <p style="text-align: right;"><a class="code-link" href="https://github.com/hallmanm" target="_blank"><img src="/images/logos/github.png"/></a></p>
+        <p style="text-align: right;"><a class="code-link" href="https://github.com/hallmanm/portfolio/tree/main/projects/live-your-life" target="_blank"><img src="/images/logos/github.png"/></a></p>
         <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/markdown.php"); ?>
       </div>
     </div>
--- projects/personalization/index.php
+++ projects/personalization/index.php
@@ -12,14 +12,14 @@
   
 </div>
 
-<div class="section">
+<!-- <div class="section">
   <h2>More Info...</h2>
   <div class="row section">
     <div class="col-xs-12">
       <p style="text-align: right;"><a class="code-link" href="https://github.com/hallmanm" target="_blank"><img src="/images/logos/github.png"/></a></p>
-      <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/markdown.php"); ?>
+      
     </div>
   </div>
-</div>
+</div> -->
 
 <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/foot.php"); ?>
