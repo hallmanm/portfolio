c098d0289b37129c28010f72de954cd3f00c68b2
Michael Hallman <1537945+hallmanm@users.noreply.github.com>
2023-12-01 09:45:21 -0500

    Add files via upload

---
+++ projects/index.php
@@ -0,0 +1,84 @@
+<?php
+  $title = "Projects";
+  include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/head.php");
+?>
+
+<h1>Projects</h1>
+<div class="section">
+  <h2>AEO</h2>
+  <div class="display"><img src="/images/displays/ae.png"/></div>
+  <div class="row sub-section">
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/stop-motion">
+        <img src="/projects/images/thumbs/jeanguide.jpg"/>
+        <span>Stop Motion Animation</span>
+      </a>
+    </div>
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/headless-cms">
+        <img src="/projects/images/thumbs/brandcardinator.jpg"/>
+        <span>Headless CMS</span>
+      </a>
+    </div>
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/template-generator">
+        <img src="/projects/images/thumbs/mastermodule.jpg"/>
+        <span>Master Module</span>
+      </a>
+    </div>
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/live-your-life">
+        <img src="/projects/images/thumbs/liveyourlife.jpg"/>
+        <span>#LiveYourLife Feature</span>
+      </a>
+    </div>
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/personalization">
+        <img src="/projects/images/thumbs/p13n.jpg"/>
+        <span>Personalized Features</span>
+      </a>
+    </div>
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/parallax">
+        <img src="/projects/images/thumbs/parallax.jpg"/>
+        <span>Parallax</span>
+      </a>
+    </div>
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/panorama">
+        <img src="/projects/images/thumbs/pano.jpg"/>
+        <span>Panorama</span>
+      </a>
+    </div>
+    <!-- <div clcard-link ass="col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/carousel">
+        <img src="/projects/images/thumbs/carousel.jpg"/>
+        <span>Dynamic Carousel</span>
+      </a>
+    </div> -->
+    <div class="card-link col-xs-6 col-sm-4 col-md-3">
+      <a href="/projects/regex">
+        <img src="/projects/images/thumbs/77kids.jpg"/>
+        <span>RegEx Word Count</span>
+      </a>
+    </div>
+  </div>
+</div>
+<div class="section">
+  <h2>Urban Impact</h2>
+  <div class="display"><img src="/images/displays/urban.png"/></div>
+</div>
+<div class="section">
+  <h2>Calvary United Methodist Church</h2>
+  <div class="display"><img src="/images/displays/calvary.png"/></div>
+</div>
+<div class="section">
+  <h2>Meeter Feeder</h2>
+  <div class="display"><img src="/images/displays/meterfeeder.png"/></div>
+</div>
+<div class="section">
+  <h2>Miss DJ Flow</h2>
+  <div class="display"><img src="/images/displays/djflow.png"/></div>
+</div>
+      
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . "/includes/foot.php"); ?>
