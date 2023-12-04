93ab6ac66db2d8ae65e797d72a38e5e12d66e8d9
Michael Hallman <1537945+hallmanm@users.noreply.github.com>
2023-12-04 08:47:21 -0500

    adding og image support

--- includes/head.php
+++ includes/head.php
@@ -10,15 +10,25 @@
     <meta name="theme-color" content="#ffffff">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    
+    <meta property="og:image" content="https://michaelwhallman.com/images/logos/og.jpg"/>
+    <meta property="og:title" content="Michael W Hallman | Sr Engineering Manager"/>
+    <meta property="og:description" content="Experienced Data-driven Engineering Manager | Expertise in Experimentation, Personalization, and Front-end Development"/>
+    <meta property="og:image:width" content="1200"/>
+    <meta property="og:image:height" content="630"/>
+    
     <title><?php if (isset($title)) echo $title." | "; ?>Michael W Hallman | Sr Engineering Manager</title>
     <meta name="description" content="Experienced Data-driven Engineering Manager | Expertise in Experimentation, Personalization, and Front-end Development">
+    
     <?php if (!isset($exclude_bootstrap)):  ?>
       <link rel="stylesheet" href="/includes/css/bootstrap-theme.min.css">
       <link rel="stylesheet" href="/includes/css/bootstrap.min.css">
     <?php endif; ?>
     <link rel="stylesheet" href="/includes/css/main.css">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-YCHET003YV"></script>
     <script>
